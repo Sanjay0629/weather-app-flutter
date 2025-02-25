89941169d6520fe7e20960b2fe884abb5476d27f
Sanjay0629 <sanjaysivaprakash86@gmail.com>
2025-02-25 23:28:45 +0530

    updated part3

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -6,16 +6,28 @@ import 'package:weather_app/weather_forecast_screen.dart';
 import 'package:weather_app/additional_info.dart';
 import 'package:http/http.dart' as http;
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeaather();
+  }
 
   Future getCurrentWeaather() async {
     String cityName = "London";
-    http.get(
+    final res = await http.get(
       Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?q=$cityName&APPID=$openWeatherAPIKey",
       ),
     );
+    print(res.body);
   }
 
   @override
